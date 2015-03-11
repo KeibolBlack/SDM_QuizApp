54b944160d5ca9bd9b49dc6ed48c62135afab161
carfrrui <carfrrui@inf.upv.es>
2015-03-11 20:06:20 +0100

    Menu Principal

--- app/src/main/java/com/example/administrador/quizapp/ScoresActivity.java
+++ app/src/main/java/com/example/administrador/quizapp/ScoresActivity.java
@@ -1,33 +1,25 @@
 package com.example.administrador.quizapp;
 
-import android.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class QuizApp_AcBar extends ActionBarActivity {
+public class ScoresActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_quizapp_acbar);
-
-        ActionBar actionBar = getActionBar();
-        actionBar.setIcon(R.drawable.ic_launcher);
-        actionBar.setTitle(R.string.acBar_Title); //Establecer titulo
-        actionBar.setSubtitle(R.string.acBar_Subtitle); //Establecer Subtitulo
-
-        actionBar.setDisplayHomeAsUpEnabled(true);
+        setContentView(R.layout.activity_scores);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_quizapp_acbar, menu);
-        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_scores, menu);
+        return true;
     }
 
     @Override
@@ -35,18 +27,7 @@ public class QuizApp_AcBar extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-
         int id = item.getItemId();
-        switch
-                (item.getItemId())
-        {
-            case R.id.action_search:
-                    // Do  something, like starting an activity, for instance
-                return true;
-            case  R.id.action_scores:
-            // Do another thing
-                return true;
-        }
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
