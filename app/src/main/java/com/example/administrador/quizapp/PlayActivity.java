dfd4606003ebb03bb09bef7238d8c980e86e6b4f
alcarrui <bertocr@gmail.com>
2015-03-12 23:41:24 +0100

    XML de PlayActivity - 2

--- app/src/main/java/com/example/administrador/quizapp/PlayActivity.java
+++ app/src/main/java/com/example/administrador/quizapp/PlayActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
@@ -23,6 +24,11 @@ import static com.example.administrador.quizapp.R.menu.play_menu;
  * Created by AlbertoCR on 11/03/2015.
  */
 public class PlayActivity extends ActionBarActivity{
+
+    ArrayList<Question> listaPreguntas = new ArrayList<Question>();
+    Question pregunta = null;
+    EditText et;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -31,13 +37,17 @@ public class PlayActivity extends ActionBarActivity{
         actionBar.setDisplayHomeAsUpEnabled(true);
 
         leerXML();
+        cargarPregunta();
 
     }
 
-    private void leerXML() {
-        ArrayList<Question> listaPreguntas = null;
-        Question pregunta = null;
+    private void cargarPregunta() {
+        pregunta = listaPreguntas.get(0);
+        ((EditText)findViewById(R.id.question)).setText(pregunta.text);
+        ((EditText)findViewById(R.id.answer1)).setText(pregunta.answer1);
+    }
 
+    private void leerXML() {
         try{
             BufferedReader _fin = new BufferedReader(new InputStreamReader(openFileInput("questions-1.xml")));
 
@@ -48,15 +58,25 @@ public class PlayActivity extends ActionBarActivity{
 
             while (tagType != XmlPullParser.END_DOCUMENT){
                 if (tagType == XmlPullParser.START_TAG){
-
-
+                    pregunta.setNumber(_parser.getAttributeValue(null, "number"));
+                    pregunta.setText(_parser.getAttributeValue(null, "text"));
+                    pregunta.setAnswer1(_parser.getAttributeValue(null, "answer1"));
+                    pregunta.setAnswer2(_parser.getAttributeValue(null, "answer2"));
+                    pregunta.setAnswer3(_parser.getAttributeValue(null, "answer3"));
+                    pregunta.setAnswer4(_parser.getAttributeValue(null, "answer4"));
+                    pregunta.setRight(_parser.getAttributeValue(null, "right"));
+                    pregunta.setAudience(_parser.getAttributeValue(null, "audience"));
+                    pregunta.setPhone(_parser.getAttributeValue(null, "phone"));
+                    pregunta.setFifty1(_parser.getAttributeValue(null, "fifty1"));
+                    pregunta.setFifty2(_parser.getAttributeValue(null, "fifty2"));
                 }
 
+                listaPreguntas.add(pregunta);
                 _parser.next();
             }
 
+            _fin.close();
         } catch (Exception e){}
-
     }
 
 
